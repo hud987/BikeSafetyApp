0b04dc909b7138f8855ebd407da66662d58f0389
Michael Hudachek <hud987@gmail.com>
2020-02-06 19:11:50 -0500

    add direction line on map

--- screens/Driver.js
+++ screens/Driver.js
@@ -9,17 +9,20 @@ import {
 } from "react-native";
 import MapView, { PROVIDER_GOOGLE } from 'react-native-maps';
 import RNLocation from 'react-native-location';
+import Geolocation from 'react-native-geolocation-service';
+import Polyline from '@mapbox/polyline'
 
 export default class BikerOrDriver extends React.Component {
   state = {
     ready: false,
-    where: {
-      lat: null,
-      lng: null,
-      speed: 0,
-      heading: 0,
-    },
+    lat: null,
+    lng: null,
+    speed: 0,
+    heading: 0,
     error: null,
+    desLat: 37.885874,
+    desLng: -122.506447
+
   }
 
   componentDidMount() {
@@ -29,11 +32,11 @@ export default class BikerOrDriver extends React.Component {
       maxAge: 60*60
     }
     this.setState({ready: false, error: null})
-
+    
     RNLocation.configure({
-      distanceFilter: 5.0
+      distanceFilter: 0
     })
-
+    console.log('did mount')
     RNLocation.requestPermission({
       ios: "whenInUse",
       android: {
@@ -41,9 +44,16 @@ export default class BikerOrDriver extends React.Component {
       }
     }).then(granted => {
         if (granted) {
-          this.locationSubscription = RNLocation.subscribeToLocationUpdates(locations => {})
-        }
-      })
+          this.locationSubscription = RNLocation.subscribeToLocationUpdates(locations => {
+            console.log('locations')
+            console.log('location')
+            this.setState({
+              lat: locations[0].latitude,
+              lng: locations[0].longitude
+            }, this.mergeCoords)
+        })
+      }
+    })  
   }
 
   geoSuccess = (position) => {
@@ -61,6 +71,45 @@ export default class BikerOrDriver extends React.Component {
     this.setState({error: err.message})
   }
 
+  mergeCoords = () => {
+    console.log('merging coords')
+    console.log('lat')
+    console.log(this.state.lat)
+    console.log('lng')
+    console.log(this.state.lng)
+    lat = this.state.lat
+    lng = this.state.lng
+    desLat = this.state.desLat
+    desLng = this.state.desLng
+
+    const hasStartAndEnd = lat != null && desLat != null
+    if (hasStartAndEnd) {
+      const concatStart = `${lat},${lng}`
+      const concatEnd = `${desLat},${desLng}`
+      this.getDirections(concatStart, concatEnd)
+    }
+  }
+
+  async getDirections(startLoc, desLoc) {
+    console.log('getting directions')
+    try {
+      const resp = await fetch(`https://maps.googleapis.com/maps/api/directions/json?origin=${startLoc}&destination=${desLoc}&key=AIzaSyAQ6JZnM-5sMY9Za63OUKE2fNvRQy_h9CY`)
+      console.log('resp')
+      const respJson = await resp.json()
+      const points = Polyline.decode(respJson.routes[0].overview_polyline.points)
+      const coords = points.map(point => {
+        return {
+          latitude : point[0],
+          longitude : point[1]
+        }
+      })
+      
+      this.setState({ coords },console.log(this.state.coords))
+    } catch(error) {
+      console.log('error: ' + error)
+    }
+  }
+
   render() {
     return(
       <MapView
@@ -73,7 +122,13 @@ export default class BikerOrDriver extends React.Component {
           latitudeDelta: 0.0922,
           longitudeDelta: 0.0421,
         }}
-      />
+      >
+        <MapView.Polyline 
+          strokeWidth = {2}
+          strokeColor = {'red'}
+          coordinates = {this.state.coords}
+        />
+      </MapView>
     );
   }
 };
